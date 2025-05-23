820cfd24452b969f0ad07d6c17af00f4992d048a
Doobiesuckin <123426804+BigBongTheory42@users.noreply.github.com>
2025-05-23 01:46:38 -0500

    Add files via upload

---
+++ SideWinder_TornCity_Doobiesuckin_1.1/popup.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Function to update theme and storage
+    function updateTheme(theme) {
+        document.body.classList.toggle('dark-theme', theme === 'dark');
+        chrome.storage.local.set({ 'sidebarTheme': theme });
+    }
+
+    // Check if we're on Torn's page
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const isTornPage = tabs[0]?.url?.includes('torn.com');
+        
+        // Check storage first
+        chrome.storage.local.get(['sidebarTheme'], (result) => {
+            if (result.sidebarTheme && !isTornPage) {
+                // Use saved theme if not on Torn
+                updateTheme(result.sidebarTheme);
+            } else if (isTornPage) {
+                // Check page theme if on Torn
+                if (!tabs[0]?.id) return;
+                
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: checkTheme
+                }, (results) => {
+                    if (results?.[0]?.result) {
+                        const theme = results[0].result;
+                        updateTheme(theme);
+                    }
+                });
+            } else {
+                // Default to light theme
+                updateTheme('light');
+            }
+        });
+    });
+
+    // Set version
+    fetch(chrome.runtime.getURL('manifest.json'))
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("version").textContent = data.version;
+        })
+        .catch(error => console.error('Error fetching version:', error));
+
+    // Rate button
+    document.getElementById("rateBtn")?.addEventListener("click", () => {
+        chrome.tabs.create({ url: "https://chrome.google.com/webstore/detail/jcglaodacielmmbaphkljgmdaejbndci/reviews" });
+    });
+
+    // Form button
+    document.getElementById("formButton")?.addEventListener("click", () => {
+            chrome.tabs.create({ url: "https://torn.com/index.php" });
+    });
+
+});
+
+function checkTheme() {
+    const groupContainer = document.getElementById('enhanced-sidebar');
+    if (groupContainer) {
+        const color = getComputedStyle(groupContainer).backgroundColor;
+        const theme = color === 'rgb(26, 26, 26)' ? 'dark' : 'light';
+        return theme;
+    }
+    return 'light'; // Default theme
+}
